6e7b367be5552ec2ef1a37dc1fb06cc5b895150c
Shyamala27 <desaraju.shyamala@gmail.comShyamala27>
2025-03-19 20:58:58 -0400

    ManageClassFeature

--- tests/PageObjects/Class.js
+++ tests/PageObjects/Class.js
@@ -11,15 +11,15 @@ export class ClassPage{
         this.passwordInput = page.locator("#password");
         this.loginbutton =page.locator("xpath=//span[text()='Login']");
         this.classBtn = page.locator("//span[normalize-space()='Class']");
-        //this.LMSTitle = page.getByText(' LMS - Learning Management System ')
         this.ManageClassHeader = page.getByText(' Manage Class')
         this.SearchBar = page.locator("//input[@id='filterGlobal']")
-        this.DataTableHeaders = page.locator("//thead[@class='p-datatable-thead']")
-       
-        this.TextPagination = page.locator("//span[@class='p-paginator-current ng-star-inserted']")
-        this.deleteButton = page.locator("//button[@class='p-button-danger p-button p-component p-button-icon-only']//span[@class='p-button-icon pi pi-trash']")
-         //this.DeleteButton = page.locator("xpath=//button//span[@class= 'p-button-icon pi pi-trash']")
-       // this.sortIcon = page.locator("//p-sorticon[@field='batchName']//i[@class='p-sortable-column-icon pi pi-fw pi-sort-alt']")
+        this.DataTableHeaders = page.locator("xpath=/html/body/app-root/app-session/div/mat-card/mat-card-content/p-table/div/div[1]/table/thead");
+       this.TextPagination =  page.locator("//div[@class='p-d-flex p-ai-center p-jc-between ng-star-inserted']")
+       this.deleteButton = page.locator("//button[@class='p-button-danger p-button p-component p-button-icon-only']//span[@class='p-button-icon pi pi-trash']")
+        this.allRows = page.locator("xpath = //table/tbody/tr")
+        this.sortIcon = page.locator("//div//table//thead//i[@class='p-sortable-column-icon pi pi-fw pi-sort-alt']")
+        this.totalNoClasses = page.locator("xpath=/html/body/app-root/app-session/div/mat-card/mat-card-content/p-table/div/div[2]/div");
+
 
     }
 
@@ -57,6 +57,7 @@ export class ClassPage{
     }
 
     async ManageClass_Header() {
+        
 
     await expect(this.ManageClassHeader).toBeVisible();
 
@@ -69,18 +70,28 @@ export class ClassPage{
     }
 
     async DataTable_Headers() {
-
-
-     const DtHeaders = await this.DataTableHeaders.allInnerTexts();
-     console.log("DataTable Headers:", DtHeaders);
-     return DtHeaders.map(header => header.trim());
+    
+        await this.DataTableHeaders.first().waitFor({ state: "visible" });
+        const headers = await this.DataTableHeaders.all();
+        // Initialize an array to store the header texts
+        let headerTexts = [];
+    
+        for (const header of headers) {
+            const text = await header.textContent();
+            // If there's text, push it to the headerTexts array (trim to remove extra spaces)
+            if (text) headerTexts.push(text.trim()); // Remove extra spaces
+        }
+    
+        console.log("Actual Headers:", headerTexts);
+        return headerTexts;
 
     }
 
    async Text_Pagination() {
-
-    await expect( this.TextPagination).toBeVisible();
-    await expect( this.TextPagination).toBeEnabled();
+     //await this.page(this.Text_Pagination).getByText();
+     await expect( this.TextPagination).toBeVisible();
+    
+    //await expect( this.TextPagination).toBeEnabled();
 
 
 
@@ -88,17 +99,32 @@ export class ClassPage{
 
    async Sorticon_Elements() {
 
-    return await this.DataTableHeaders.all()
+    //return await this.DataTableHeaders.all()
+    const rowCount = await this.allRows.count();
+    const sortIconCount = await this.sortIcon.count();
+    return sortIconCount === rowCount;
 
    }
 
 async Delete_Button() {
 
-await expect(this.deleteButton).toBeVisible();
+const Deletebutton = await expect(this.deleteButton).toBeVisible();
+return Deletebutton;
 //await this.page(this.DeleteButton).toBeEnabled();
 
 }
 
+async TotalPages_Text(){
 
+     return await this.totalNoClasses.innerText();
+   // await this.page(this.totalNoClasses).toBeVisible();
+    
+}
+
+// async TotalClasses_Displayed() {
+
+//    return (this.totalNoClasses);
+
+//   }
 
 }
